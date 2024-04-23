332c9502a7d2bab91ad34fd363d0c8dfe8b3239c
Valentin <vtonkopey@gmail.com>
2024-04-23 12:48:48 +0300

    add logic table

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,30 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 
 export class Header extends ExcelComponent {
-	static className = 'excel__header'
+  static className = "excel__header";
 
-	toHTML() {
-		return `
+  constructor($root, options) {
+    super($root, {
+      name: "Header",
+      ...options,
+    });
+  }
+
+  toHTML() {
+    return `
 		<input type="text" class="input" value="Новая таблица" />
 
 		<div>
-  
+
 		  <div class="button">
 			<i class="material-icons">delete</i>
 		  </div>
-  
+
 		  <div class="button">
 			<i class="material-icons">exit_to_app</i>
 		  </div>
-  
+
 		</div>
-		`
-	}
+		`;
+  }
 }
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,24 +1,87 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 import { createTable } from "./table.template";
 import { resizeHandler } from "./table.resize";
+import { isCell, matrix, nextSelector, shouldResize } from "./table.functions";
+import { $ } from "../../core/dom";
+import { TableSelection } from "./TableSelection";
 
 export class Table extends ExcelComponent {
-	static className = 'excel__table'
+  static className = "excel__table";
 
-	constructor($root) {
-		super($root, {
-			listeners: ['mousedown']
-		})
-	}
+  constructor($root, options) {
+    super($root, {
+      name: "Table",
+      listeners: ["mousedown", "keydown", "input"],
+      ...options,
+    });
+  }
 
-	toHTML() {
-		return createTable(20)
-	}
+  toHTML() {
+    return createTable(20);
+  }
 
-	onMousedown(event) {
-		if (event.target.dataset.resize) {
-			resizeHandler(this.$root, event)
-		}
-	}
+  prepare() {
+    this.selection = new TableSelection();
+  }
 
+  selectCell($cell) {
+    this.selection.select($cell);
+    this.$emit("table:select", $cell);
+  }
+
+  init() {
+    super.init();
+
+    this.selectCell(this.$root.find('[data-id="0:0"]'));
+
+    this.$on("formula:input", (text) => {
+      this.selection.current.text(text);
+      console.log("Table from Formuls", text);
+    });
+
+    this.$on("formula:done", () => {
+      this.selection.current.focus();
+    });
+  }
+
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
+    } else if (isCell(event)) {
+      const $target = $(event.target);
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current).map((id) =>
+          this.$root.find(`[data-id="${id}"]`),
+        );
+
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
+    }
+  }
+
+  onKeydown(event) {
+    const keys = [
+      "Enter",
+      "Tab",
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowLeft",
+      "ArrowRight",
+    ];
+    const { key } = event;
+
+    if (keys.includes(key) && !event.shiftKey) {
+      event.preventDefault();
+      const id = this.selection.current.id(true);
+      const $next = this.$root.find(nextSelector(key, id));
+
+      this.selectCell($next);
+    }
+  }
+
+  onInput(event) {
+    this.$emit("table:input", $(event.target));
+  }
 }
